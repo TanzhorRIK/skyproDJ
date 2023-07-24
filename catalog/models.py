d36f355c687c7c0359232773e1e4c70c4f9ab0e6
Tanzhor_RIK <tanzhor4@gmail.com>
2023-07-25 01:04:27 +0700

    add two models(Product, Category)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=140, verbose_name="имя")
+    description = models.TextField(verbose_name="описание")
+    preview = models.ImageField(verbose_name="изображение")
+    category = models.CharField(max_length=140, verbose_name="категория")
+    price = models.FloatField(verbose_name="цена")
+    creation_date = models.DateField(auto_now_add=True, verbose_name="дата создания")
+    date_of_change = models.DateField(auto_now=True, verbose_name="дата последнего изменения")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=140, verbose_name="имя")
+    description = models.TextField(verbose_name="описание")
