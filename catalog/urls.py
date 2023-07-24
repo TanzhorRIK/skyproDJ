6bc6e8a4ca1b6562f5cda19c6288f4de92c404c1
Tanzhor_RIK <tanzhor4@gmail.com>
2023-07-25 02:37:24 +0700

    add DEBUG urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import index, contacts
+from config import settings
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts)
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
