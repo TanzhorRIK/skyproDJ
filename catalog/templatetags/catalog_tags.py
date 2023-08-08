9e989b2a08770502801eb876d44f0613170c7a2d
Tanzhor_RIK <tanzhor4@gmail.com>
2023-08-08 22:26:03 +0700

    add tag

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -12,3 +12,13 @@ def mediapath(object_img):
 @register.simple_tag
 def mediapath(object_img):
     return f'/media/{object_img}'
+
+
+@register.filter
+def id_path(id):
+    return f'/card/{id}'
+
+
+@register.simple_tag
+def id_path(id):
+    return f'/card/{id}'
