bb20b7654eb88b55223d2afcf81ffd6bf116fced
Tanzhor_RIK <tanzhor4@gmail.com>
2023-08-12 03:31:41 +0700

    add urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,22 @@
 from django.conf.urls.static import static
 from django.urls import path
+from catalog.apps import CatalogConfig
 
-from catalog.views import index, contacts, card
+from catalog.views import card, IndexListView, ContactsListView, BlogCreateView, \
+    BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 from config import settings
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', index, name="home"),
-    path('contacts/', contacts, name="contacts"),
-    path('card/<int:pk>', card, name="card")
+    path('', IndexListView.as_view(), name="home"),
+    path('contacts/', ContactsListView.as_view(), name="contacts"),
+    path('card/<int:pk>', card, name="card"),
+    path('blog/create/', BlogCreateView.as_view(), name="add_blog"),
+    path('blogs/', BlogListView.as_view(), name="blogs"),
+    path('blog/<slug:slug>/', BlogDetailView.as_view(), name="blog"),
+    path('blog/update/<slug:slug>/', BlogUpdateView.as_view(), name="update_blog"),
+    path('blog/delete/<slug:slug>/', BlogDeleteView.as_view(), name="delete_blog")
 ]
 
 if settings.DEBUG is True:
