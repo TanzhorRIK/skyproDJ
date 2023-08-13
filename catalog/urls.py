b2231c62fb9d64d1ac18e8721930521b0c3e8aa6
Tanzhor_RIK <tanzhor4@gmail.com>
2023-08-14 05:30:08 +0700

    add urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from catalog.apps import CatalogConfig
 
 from catalog.views import ProductListView, ContactsListView, BlogCreateView, \
     BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, \
-    ProductDetailView
+    ProductDetailView, VersionCreateView, ProductCreateView
 from config import settings
 
 app_name = CatalogConfig.name
@@ -12,12 +12,14 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name="home"),
     path('contacts/', ContactsListView.as_view(), name="contacts"),
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
     path('card/<int:pk>', ProductDetailView.as_view(), name="card"),
-    path('blog/create/', BlogCreateView.as_view(), name="add_blog"),
+    path('add_blog/', BlogCreateView.as_view(), name="add_blog"),
     path('blogs/', BlogListView.as_view(), name="blogs"),
     path('blog/<slug:slug>/', BlogDetailView.as_view(), name="blog"),
     path('blog/update/<slug:slug>/', BlogUpdateView.as_view(), name="update_blog"),
-    path('blog/delete/<slug:slug>/', BlogDeleteView.as_view(), name="delete_blog")
+    path('blog/delete/<slug:slug>/', BlogDeleteView.as_view(), name="delete_blog"),
+    path('version/', VersionCreateView.as_view(), name='add_version')
 ]
 
 if settings.DEBUG is True:
